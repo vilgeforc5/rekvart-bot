3a1d4423e8ffd94703a1a8d7d466ee1a740af3a9
Boris Belobrov <boris.belobrov@aporia957.onmicrosoft.com>
2025-11-22 21:57:00 +0300

    change vite config env

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,9 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  envDir: resolve(__dirname, "../../"),
 });
